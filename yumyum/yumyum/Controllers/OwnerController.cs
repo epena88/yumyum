d2d02fb6c471dcaab62bdf08e151afb4799bd360
emmanuel peña flores <emmanuel.pf88@gmail.com>
2015-03-19 00:47:45 -0600

    Add MongoDB

---
+++ yumyum/yumyum/Controllers/OwnerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using yumyum.Models;
+using mdb = yumyum.Data;
+
+namespace yumyum.Controllers
+{
+    public class OwnerController : ApiController
+    {
+        public HttpResponseMessage Post(Owner owner)
+        {
+            owner.Id = mdb.MongoDB.GetNewId();
+            new mdb.MongoDB().AddItem<Owner>("owner", owner);
+            var response = Request.CreateResponse<Owner>(HttpStatusCode.Created, owner);
+            string uri = Url.Link("DefaultApi", new { id = owner.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        public List<Owner> Get()
+        {
+            List<Owner> owners = new mdb.MongoDB().GetAllItems<Owner>("owner");
+            return owners;
+        }
+    }
+}
---
+++ yumyum/yumyum/Models/Owner.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yumyum.Models
+{
+    public class Owner
+    {
+        public ObjectId Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Gender")]
+        public string Gender { get; set; }
+
+        [BsonElement("Mail")]
+        public string Mail { get; set; }
+
+        [BsonElement("Password")]
+        public string Password { get; set; }
+
+        [BsonElement("Phone")]
+        public string Phone { get; set; }
+    }
+}
