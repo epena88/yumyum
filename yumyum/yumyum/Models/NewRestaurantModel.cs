bc12a0d89c6803e0d82d231b0f806874f940c5e7
emmanuel peña flores <emmanuel.pf88@gmail.com>
2015-06-01 23:34:34 -0500

    restaurant

--- yumyum/yumyum/Models/NewRestaurantModel.cs
+++ yumyum/yumyum/Models/NewRestaurantModel.cs
@@ -10,18 +10,18 @@ namespace yumyum.Models
         public string MailOwner { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Phone { get; set; }
+        //public string Phone { get; set; }
         //public string Category { get; set; }
         public string Logo { get; set; }
-        public int DeliveryTime { get; set; }
+        //public int DeliveryTime { get; set; }
         public string Country { get; set; }
         public string Estate { get; set; }
         public string City { get; set; }
-        public string Address { get; set; }
-        public string Lat { get; set; }
-        public string Long { get; set; }
-        public double ShippingCost { get; set; }
-        public double ServiceRange { get; set; }
+        //public string Address { get; set; }
+        //public string Lat { get; set; }
+        //public string Long { get; set; }
+        //public double ShippingCost { get; set; }
+        //public double ServiceRange { get; set; }
         public string AccessToken { get; set; }
     }
 }
