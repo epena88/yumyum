8ed15c0b61065be42efac710ead1ba20bb9fbbea
emmanuel peña flores <emmanuel.pf88@gmail.com>
2015-06-01 22:18:57 -0500

    ClientID

---
+++ yumyum/yumyum/Models/ClientID.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace yumyum.Models
+{
+    [BsonIgnoreExtraElements]
+    public class ClientID
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Client_Id { get; set; }
+        public string Secret_Key { get; set; }
+        public string Client_Mobile { get; set; }
+
+        public async Task<bool> Validate(string client_id, string secret_key)
+        {
+            var mongoContext = new Models.MongoContext();
+            var client = await mongoContext.ClientIDs.Find(x => x.Client_Id == client_id && x.Secret_Key == secret_key).FirstOrDefaultAsync();
+
+            return client != null;
+        }
+    }
+}
---
+++ yumyum/yumyum/Startup - copia.cs
@@ -0,0 +1,43 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Infrastructure;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using yumyum.Providers;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(yumyum.Startup))]
+namespace yumyum
+{
+    public class Startup
+    {
+        public static OAuthBearerAuthenticationOptions OAuthBearerOptions { get; private set; }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseOAuthAuthorizationServer(new OAuthAuthorizationServerOptions { 
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/Oauth"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(1),
+                Provider = new AuthorizationServerProvider(),
+                RefreshTokenProvider = new RefreshTokenProvider()
+            });
+
+            OAuthBearerOptions = new OAuthBearerAuthenticationOptions
+            {
+                AuthenticationType = "Bearer",
+                AuthenticationMode = Microsoft.Owin.Security.AuthenticationMode.Active
+            };
+
+            app.UseOAuthBearerAuthentication(OAuthBearerOptions);
+
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+
+        }
+    }
+}
